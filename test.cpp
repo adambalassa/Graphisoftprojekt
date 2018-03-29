4e3514231d943ed972cd0619411342daf6f0d99d
vagobenedek <37839178+vagobenedek@users.noreply.github.com>
2018-03-29 12:00:30 +0200

    random koordinate

--- test.cpp
+++ test.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
 
-int main(){
-  std::cout << "Hello world!" << std::endl; //asd
-  return 0;
+
+using namespace std;
+
+int main()
+{
+    srand (time(NULL));
+    int c=10;
+    int x[c];
+    int y[c];
+
+    for (int i=0; i!=c; i++)
+    {
+        int a=rand()%2;
+        if (a==0)
+            {a=-1;}
+        x[i]=((rand() % 32767)*(rand() % 32767))*a;
+
+        int b =rand()%2;
+        if (b==0)
+            {b=-1;}
+        y[i]=((rand() % 32767)*(rand() % 32767))*b;
+        cout<<"City"<<i+1<<": "<<x[i]<<"; "<<y[i]<<endl;
+    }
+    return 0;
 }
